fc2f2f3eed1b46517591d3fcb1396271c039c13b
ninjajosh <jwolabaugh@gmail.com>
2017-07-10 13:52:03 +0200

    adding ignores

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ console.log('Botty Bot Bot is live');
 
 var Twit = require('twit');
 
-//I should take these out later cuz github lawls.
-var T = new Twit({
-    consumer_key:         'esd3u48TKL0iaXmAsAMUVm7wo',
-    consumer_secret:      'dVPqzi9is9la96nY03O2jBcScPOETODZ0BWO7dNHPa7swrIqAL',
-    access_token:         '884142774462935041-k06sdmkd62K1LGRZqlr05ww9MVSXRlH',
-    access_token_secret:  'CTZhJQ8vqh4I6HhDcmT52Q9NLYovKyll2RsmA0hWaeEs2',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+var config = require('./config');
+
+console.log(config);
+
+var T = new Twit(config);
 
 T.post('statuses/update', { status: 'Bot Is Go!' }, function(err, data, response) {
     console.log(data)
-})
+});
